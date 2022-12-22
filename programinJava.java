39ff2cc1f9a4adff615f0dcd45ae2061560a1b1a
Ari24UM <121136945+Ari24UM@users.noreply.github.com>
2022-12-22 17:49:48 -0600

    Add files via upload

---
+++ programinJava.java
@@ -0,0 +1,59 @@
+public class Main {
+  public static void main(String[ ]args) {
+    String numerolf = "positivo, negativo, 0";
+    If (numerolf > 0 "positivo"){   
+        if (numerolf < 0 "negativo")
+        System.out.println( "Es positivo, negativo")
+    } else { 
+        System.out.println("Es 0")
+    }
+
+    Var contador < 3
+     Mientras (contador < 3) Bucle While 
+     Suma uno al contador >       
+     Int contador < 3;
+     While contador menor que 3) { 
+        System.out.println(contador)
+        Contador -= Contador -6
+     }
+< 
+DO { 
+    SYSTEM.OUT.PRINTLN (CONTADOR);
+    CONTADOR = CONTADOR 2; 
+    } WHILE(CONTADOR < 3); 
+    }
+    INT CONTADOR = 0; 
+    PUBLIC STATIC VOID MAIN(STRING[] ARGS){
+    INT VALORES [ ] = { 0, 1, 2, 3}; 
+    // 0 < 3
+    // 1 < 3
+    // 2 < 3
+    // 3 < 3!!!! ESTA NO! 
+  
+    FOR (INT i = 0; 1 < contador lenght; 1) { 
+    For (int I = 0 : contador) {
+    System.out.println(contador[i]) {
+    
+        #Switch Case
+        public static void main(String[]args) {
+            var estacion = "invierno";
+            switch (estacion) {
+                case "verano":
+                System.out.println("es verano");
+                case "invierno":
+                System.out.println("es invierno");
+                case "otoño":
+                System.out.println("es otoño");
+                case "primavera":
+                System.out.println("es primavera");
+    
+                    break;
+            
+                default:
+                System.out.println("en default")
+                    
+            }
+        }
+
+    
+
